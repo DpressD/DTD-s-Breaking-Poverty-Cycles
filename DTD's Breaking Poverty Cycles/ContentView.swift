83ac17b1b2eeb2e325885cb908c1f4d4997d9a09
Beloved Joshua Simons <bjsimons@theiabt.org>
2024-03-02 21:31:25 -0500

    Initial Commit

---
+++ DTD's Breaking Poverty Cycles/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  DTD's Breaking Poverty Cycles
+//
+//  Created by Halee Simons on 3/2/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ DTD's Breaking Poverty Cycles/DTD_s_Breaking_Poverty_CyclesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DTD_s_Breaking_Poverty_CyclesApp.swift
+//  DTD's Breaking Poverty Cycles
+//
+//  Created by Halee Simons on 3/2/24.
+//
+
+import SwiftUI
+
+@main
+struct DTD_s_Breaking_Poverty_CyclesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
